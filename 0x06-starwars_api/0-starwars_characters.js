a511e4ee31f67315d533de1b0dc197a646d05c1c
Giddy-K <gideonkipamet@gmail.com>
2024-11-14 20:23:40 +0300

    Chnaged the code structure a bit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,30 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(apiUrl, (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  // Parse movie information and retrieve characters list
+  const characters = JSON.parse(body).characters;
+
+  // Request each character's information in order
+  const fetchCharacter = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) reject(err);
+        else resolve(JSON.parse(body).name);
+      });
+    });
+  };
+
+  // Use Promise.all to fetch all characters in parallel
+  Promise.all(characters.map(fetchCharacter))
+    .then((names) => names.forEach((name) => console.log(name)))
+    .catch((error) => console.error('Failed to retrieve characters:', error));
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
