afd499fa01af3f665ac14f3405bb776839c7e9c0
Giddy-K <gideonkipamet@gmail.com>
2025-01-20 01:42:19 +0300

    More changes

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,59 @@
 #!/usr/bin/python3
-""" N queens """
+"""N Queens Problem Solver"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def print_error_and_exit(message, status_code):
+    """Print error message and exit with a status code."""
+    print(message)
+    exit(status_code)
+
+
+# Input validation
+if len(sys.argv) != 2:
+    print_error_and_exit("Usage: nqueens N", 1)
 
 if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+    print_error_and_exit("N must be a number", 1)
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+N = int(sys.argv[1])
 
-n = int(sys.argv[1])
+if N < 4:
+    print_error_and_exit("N must be at least 4", 1)
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+def solve_nqueens(N):
+    """Solve the N Queens problem using backtracking."""
+    def backtrack(row, columns, diagonals1, diagonals2, board):
+        """Recursive helper to explore solutions."""
+        if row == N:
+            # Add a valid solution
+            solutions.append([pos[:] for pos in board])
+            return
+
+        for col in range(N):
+            if col not in columns and (row + col) not in diagonals1 and (row - col) not in diagonals2:
+                # Choose
+                board.append([row, col])
+                columns.add(col)
+                diagonals1.add(row + col)
+                diagonals2.add(row - col)
+
+                # Explore
+                backtrack(row + 1, columns, diagonals1, diagonals2, board)
+
+                # Un-choose
+                board.pop()
+                columns.remove(col)
+                diagonals1.remove(row + col)
+                diagonals2.remove(row - col)
+
+    solutions = []
+    backtrack(0, set(), set(), set(), [])
+    return solutions
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+# Generate and print all solutions
+solutions = solve_nqueens(N)
+for solution in solutions:
+    print(solution)
